0130069146ec70a7e0f14ad93961595cf863c8eb
Zach King <zacheking10@gmail.com>
2021-10-31 04:38:00 -0600

    got rid of unnecessary comments and console logs in cardCtrl file

--- pokefy/server/controllers/cardCtrl.js
+++ pokefy/server/controllers/cardCtrl.js
@@ -1,26 +1,22 @@
 require("dotenv").config()
-const axios = require("axios")
-const bcrypt = require("bcryptjs")
-const { clientID, clientSecret } = process.env
+
 
 module.exports = {
     getArtistCards: async (req, res) => {
         const db = req.app.get('db')
 
-        const {userId} = req.session // Gonna set username to redux store later
+        const {userId} = req.session // Gonna set username to redux store later, or maybe not. We'll see what the future holds for Chwunny
 
         const cards = await db.get_artist_cards_by_username([userId])
-        // console.log(cards)
 
         res.status(200).send(cards)
     },
     getAlbumCards: async (req, res) => {
         const db = req.app.get('db')
 
-        const {userId} = req.session // Gonna set username to redux store later
+        const {userId} = req.session
 
         const cards = await db.get_album_cards_by_username([userId])
-        // console.log(cards)
 
         res.status(200).send(cards)
     },
@@ -30,9 +26,7 @@ module.exports = {
         const { userId } = req.session
         const {name, image, artist_id, genre, popularity, followers, album_1, alb1_tracks, album_2, alb2_tracks, card_type } = req.body
 
-        const newCard = await db.create_new_artist_card([userId, name, image, artist_id, genre, card_type.type, album_1.name, alb1_tracks, album_2.name, alb2_tracks, popularity, followers, false])
-
-        // console.log(newCard);
+        await db.create_new_artist_card([userId, name, image, artist_id, genre, card_type.type, album_1.name, alb1_tracks, album_2.name, alb2_tracks, popularity, followers, false])
 
         res.status(200).send('Success')
     },
@@ -41,7 +35,6 @@ module.exports = {
 
         const { userId } = req.session
         const {artistName, artistId, albumName, tracks, image, genre, popularity, releaseDate, type} = req.body
-        // console.log(userId, req.body);
 
         await db.create_new_album_card([userId, artistName, artistId, image, genre, type, albumName, tracks, popularity, releaseDate, false])
 
